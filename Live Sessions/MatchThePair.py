842404dc211eed73e6eaa53b1c302f696a445292
TechClubPro <62166836+TechClubPro@users.noreply.github.com>
2020-06-21 16:42:01 +0530

    Add files via upload

--- Live Sessions/MatchThePair.py
+++ Live Sessions/MatchThePair.py
@@ -9,29 +9,13 @@ print("5. Abhinav Bindra   e. Badminton")
 
 ans = int(input("Please give the answer sequence in format 1a,2b,3c..."))
 
-if ans %2 ==0 and ans!=7 and ans!=2 and ans!=1:
-    print("31")
-elif ans==7:
-    print("31")
-elif ans==2:
-    print("28/29",ans)
-else:
-    print("30")
-# if ans.isalpha():
-#         print("String")
-        
-# else:
-#         print("Digits")
-        
-# if not re.search("[a-z]",ans):
-#     print("Either numbres or caps")
 
-# if ans == "1c,2a,3e,4b,5d":
+if ans == "1c,2a,3e,4b,5d":
     
    
-#     print("Correct Match")
-# else:
-#     print("Wrong Match")
+    print("Correct Match")
+else:
+    print("Wrong Match")
     
     
 
